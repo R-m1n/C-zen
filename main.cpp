894c2c7dc2ed03b43cf10306c218d1d794a2ccdb
Armin Abbasi <apmnh.armin@gmail.com>
2023-08-14 15:32:54 +0200

    new stuff

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 
 // #include "playground/wierdo-table/WeirdoTable.h"
 #include "algorithms/Sorting.h"
+#include "utility/Timer.h"
 
 void ocd::selection(std::vector<int> &vec)
 {
@@ -27,16 +28,36 @@ int ocd::min_index(const std::vector<int> &vec, int start)
     return index;
 }
 
+void ocd::insertion(std::vector<int> &vec)
+{
+    util::Timer time;
+    for (int curr_index = 0; curr_index < vec.size(); ++curr_index)
+    {
+        int current = vec[curr_index];
+
+        int prev_index = curr_index - 1;
+
+        while ((current < vec[prev_index]))
+        {
+            vec[prev_index + 1] = vec[prev_index];
+
+            --prev_index;
+        }
+
+        vec[prev_index + 1] = current;
+    }
+}
+
 int main()
 {
-    std::vector<int> vec{5, 2, 4, 10, 7, 1, 6};
+    std::vector<int> vec{1, 2, 4, 10, 7, 5, 6};
 
-    ocd::selection(vec);
+    ocd::insertion(vec);
 
-    for (auto i : vec)
-    {
-        std::cout << i << '\n';
-    }
+    // for (auto i : vec)
+    // {
+    //     std::cout << i << '\n';
+    // }
 
     // for (int i = 0; i < vec.size(); ++i)
     //     std::cout << ocd::min(vec, i) << '\n';
---
+++ utility/Timer.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <chrono>
+#include <iostream>
+
+namespace util
+{
+    class Timer
+    {
+    public:
+        Timer() : start{std::chrono::high_resolution_clock::now()} {}
+
+        ~Timer()
+        {
+            std::chrono::duration<double, std::milli> duration = std::chrono::high_resolution_clock::now() - start;
+
+            std::cout << duration.count() * 1000 << '\n';
+        }
+
+    private:
+        std::chrono::system_clock::time_point start;
+    };
+}
