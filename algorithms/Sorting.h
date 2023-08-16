aa79d69acb63442d6f7943a0cd5641c97f5246c4
Armin Abbasi <apmnh.armin@gmail.com>
2023-08-16 18:02:01 +0200

    merge sort added

--- algorithms/Sorting.h
+++ algorithms/Sorting.h
@@ -7,4 +7,5 @@ namespace ocd
     void bubble(std::vector<int> &vec);
     void selection(std::vector<int> &vec);
     void insertion(std::vector<int> &vec);
+    std::vector<int> merge(std::vector<int> vec);
 }
--- main.cpp
+++ main.cpp
@@ -2,47 +2,121 @@
 #include <string>
 #include <vector>
 #include <utility>
+#include <cmath>
 
 // #include "playground/wierdo-table/WeirdoTable.h"
 #include "algorithms/Sorting.h"
 #include "utility/Timer.h"
 
-void ocd::bubble(std::vector<int> &vec)
+std::vector<int> ocd::merge(std::vector<int> vec)
 {
-    if (!vec.size())
-        return;
+    const size_t vec_size{vec.size()};
 
-    bool is_sorted{false};
+    if (vec_size == 1)
+        return vec;
 
-    size_t sorted_items{0};
+    const size_t boundry{vec_size / 2};
 
-    while (!is_sorted)
+    std::vector<int> left;
+    left.reserve(boundry);
+    for (int i = 0; i < boundry; ++i)
+        left.emplace_back(vec[i]);
+
+    std::vector<int> right;
+    right.reserve(vec_size - boundry);
+    for (int i = boundry; i < vec_size; ++i)
+        right.emplace_back(vec[i]);
+
+    left = merge(left);
+    right = merge(right);
+
+    std::vector<int> sorted;
+    sorted.reserve(left.size() + right.size());
+
+    int left_index = 0;
+    int right_index = 0;
+
+    while (sorted.size() != left.size() + right.size())
     {
-        is_sorted = true;
+        int left_value = left[left_index];
+        int right_value = right[right_index];
 
-        for (int i = 0; i < vec.size() - sorted_items - 1; ++i)
-            if (vec[i + 1] < vec[i])
-            {
-                std::swap(vec[i], vec[i + 1]);
-                is_sorted = false;
-            }
+        // std::cout << left_value << ' ' << right_value << std::endl;
 
-        ++sorted_items;
+        if (left_index == left.size())
+        {
+            sorted.emplace_back(right_value);
+            ++right_index;
+
+            continue;
+        }
+
+        if (right_index == right.size())
+        {
+            sorted.emplace_back(left_value);
+            ++left_index;
+
+            continue;
+        }
+
+        if (left_value < right_value)
+        {
+            sorted.emplace_back(left_value);
+            ++left_index;
+
+            continue;
+        }
+
+        sorted.emplace_back(right_value);
+        ++right_index;
+    }
+
+    return sorted;
+}
+
+int min(int i, int j)
+{
+    return i <= j ? i : j;
+}
+
+void ocd::insertion(std::vector<int> &vec)
+{
+    for (int curr_index = 1; curr_index < vec.size(); ++curr_index)
+    {
+        int current = vec[curr_index];
+
+        int prev_index = curr_index - 1;
+
+        while ((current < vec[prev_index]))
+        {
+            vec[prev_index + 1] = vec[prev_index];
+
+            --prev_index;
+        }
+
+        vec[prev_index + 1] = current;
     }
 }
 
 int main()
 {
     std::vector<int> vec{10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
-    std::vector<int> vec1{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::vector<int> vec2{8, 2, 5, 1, 9, 10, 4, 3, 6, 7};
-
-    ocd::bubble(vec);
-
-    for (auto i : vec)
+    // std::vector<int> vec{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    // std::vector<int> vec{8, 2, 5, 1, 9, 10, 4, 3, 6, 7};
     {
-        std::cout << i << '\n';
+        util::Timer timer;
+        auto sorted = ocd::merge(vec);
+
+        for (auto i : sorted)
+        {
+            std::cout << i << '\n';
+        }
     }
+
+    // {
+    //     util::Timer timer;
+    //     ocd::insertion(vec);
+    // }
 
     return 0;
 }
